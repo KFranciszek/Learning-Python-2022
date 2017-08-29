2f340c5a8559827576f12b9796b11fcfa36ab949
KFranciszek <krzysztof.zernicki@gmail.com>
2017-08-29 21:02:35 +0200

    Create systemINFO.py

---
+++ systemINFO.py
@@ -0,0 +1,19 @@
+def infoSYS ():
+    import platform
+    print("Your system info "+
+          "System Operation: "+ platform.platform()+
+          "Processor: " + platform.processor()
+          
+
+
+
+
+
+
+
+
+          )
+
+
+
+infoSYS()
