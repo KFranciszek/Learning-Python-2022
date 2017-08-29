fdc9991c5b81315dbeabe6c5187dfca26d137d05
KFranciszek <krzysztof.zernicki@gmail.com>
2017-08-29 21:08:45 +0200

    Update systemINFO.py

--- systemINFO.py
+++ systemINFO.py
@@ -1,19 +1,15 @@
 def infoSYS ():
     import platform
-    print("Your system info "+
-          "System Operation: "+ platform.platform()+
-          "Processor: " + platform.processor()
+    print("Your system info \n"+
+          "System Operation: "+ platform.platform()+"\n"
+          "Processor: " + platform.processor() +"\n"
+          "Machine: " + platform.processor()+'\n'
+          "Node: " + platform.node()+ '\n'
+          'System: ' + platform.system() +'\n'
           
-
-
-
-
-
-
-
-
-          )
+            )
 
 
 
 infoSYS()
+
