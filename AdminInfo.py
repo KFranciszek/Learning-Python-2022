144b9b3943e250a824101b6ef4926a8d96819374
KFranciszek <krzysztof.zernicki@gmail.com>
2017-08-29 23:50:46 +0200

    Update AdminInfo.py

--- AdminInfo.py
+++ AdminInfo.py
@@ -1,8 +1,8 @@
-#used psutil libary
-
 def systemLIVE ():
     import psutil
     print(psutil.swap_memory())
     print(psutil.cpu_count(logical=False))
+#     for i in psutil.virtual_memory():
+#      print(i)
 systemLIVE()
 
