5f5a91d2f1b240c7107e02632340950911a8d87f
Kzernicki <kzernicki@edu.cdv.pl>
2022-11-17 00:21:36 +0100

    string ex add 2;3

---
+++ Python learning 2022_2023/ex_string/string_ex3.py
@@ -0,0 +1,15 @@
+#Write a Python program to find the first repeated word in a given string
+st = input("Enter frase:" )
+word = st.split()
+repete = set()
+if i in st.split():
+    if i in repete:
+        print(i)
+    else:
+        repete.add(i)
+
+print(repete)
+
+
+
+
