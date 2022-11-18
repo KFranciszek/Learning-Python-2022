dda39f886dd7c040a8e31760aae2673ae66f4165
Kzernicki <kzernicki@edu.cdv.pl>
2022-11-18 19:49:36 +0100

    add string ex5 v2

--- ex_string/string_ex5.py
+++ ex_string/string_ex5.py
@@ -19,3 +19,18 @@ for i in text_l:
 print(counter)
 
 
+#2
+from collections import Counter
+text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam urna elit, facilisis et erat vel, euismod eleifend diam. " \
+       "Quisque arcu nisi, consequat sit amet sapien sit amet, tristique blandit eros. Maecenas interdum nunc sed consequat tempus. " \
+       "Suspendisse pellentesque vulputate orci a elementum. Donec efficitur est turpis, id efficitur erat ullamcorper eget. " \
+       "Donec sit amet laoreet est. Cras ut tellus pulvinar purus tincidunt tincidunt. " \
+       "Aenean ut nisi turpis. Quisque laoreet metus et augue tincidunt, ac semper leo maximus. " \
+       "Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam vitae mattis leo. " \
+       "Vestibulum quis posuere nisi, vel posuere neque. " \
+       "Ut nec mollis augue. Nam pharetra mi et leo ultricies scelerisque. " \
+       "Donec elit augue, ullamcorper ac ultrices non, mollis et lectus. Nunc placerat ornare diam, sit amet vestibulum ex ornare quis."
+text_l2 = Counter(text.split())
+text_l3= Counter(text.split()).most_common(5)
+print(text_l2)
+print(text_l3)
