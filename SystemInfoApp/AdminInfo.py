7257085333780924b3e9fe30f29c37a219613756
Kzernicki <krzysztof.zernicki@gmail.com>
2022-11-24 21:59:17 +0100

    block systemInfoApp

--- SystemInfoApp/AdminInfo.py
+++ SystemInfoApp/AdminInfo.py
@@ -1,8 +1,8 @@
-def systemLIVE ():
-    import psutil
-    print(psutil.swap_memory())
-    print(psutil.cpu_count(logical=False))
+#def systemLIVE ():
+#    import psutil
+#    print(psutil.swap_memory())
+#    print(psutil.cpu_count(logical=False))
 #     for i in psutil.virtual_memory():
 #      print(i)
-systemLIVE()
+#systemLIVE()
 
