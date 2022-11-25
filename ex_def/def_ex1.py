7ab2aa9e98d3a3313a4189c7b1b1e31f94fcff8a
Kzernicki <krzysztof.zernicki@gmail.com>
2022-11-25 14:57:57 +0100

    add def_ex1.py

---
+++ ex_def/def_ex1.py
@@ -0,0 +1,7 @@
+#Write a program to create a function that takes two arguments, name and age, and print their value.
+
+def name_age (name,age):
+    answer = f'My name is {name} and I have {age} years'
+    return print(answer)
+
+name_age("Kris", 34)
