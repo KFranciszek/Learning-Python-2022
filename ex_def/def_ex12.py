c290c1440cc39204099e39d101efae39c0e2b9b0
Kzernicki <krzysztof.zernicki@gmail.com>
2022-11-30 21:15:37 +0100

    add def_ex11.py

---
+++ ex_def/def_ex12.py
@@ -0,0 +1,8 @@
+#Write a function called km_to_miles that takes kilometers as a parameter, converts it into miles and returns the result.
+
+def kil_mil(kil):
+    mil = kil * 0.62137
+    return  mil
+
+print(kil_mil(100))
+
