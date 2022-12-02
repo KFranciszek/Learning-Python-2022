146f3b5c8e6dd34755c23e3fe057ec556f3f1294
Kzernicki <krzysztof.zernicki@gmail.com>
2022-12-02 13:49:07 +0100

    add def_ex13.py

---
+++ ex_def/def_ex13.py
@@ -0,0 +1,14 @@
+#Write 2 functions named f_1 and f_2. First one takes a number as input and returns that number +5,
+# second function takes a number as input and returns first function's result multiplied by 2.
+
+def f_1 ():
+   a=int(input("Enter numer: "))
+   return a+5
+
+print(f_1())
+
+def f_2 ():
+   a = int(input("Enter numer: "))
+   return a * 5
+
+print(f_2())
