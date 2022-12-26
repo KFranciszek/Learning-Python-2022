d15920cb7d68525c3b6c514d96be1fc67b5d2774
KFranciszek <kfzmarketing@gmail.com>
2022-12-26 13:55:38 +0100

    add def_ex16 v2

--- ex_def/def_ex16.py
+++ ex_def/def_ex16.py
@@ -1,8 +1,18 @@
 ##Write a program to remove characters from a string starting from zero up to n and return a new string.
 
+#1
 def remove_str (word,x):
     print("Orgin:", word)
     remove_word = word[x:]
     print(remove_word)
 
 remove_str("Write a program to remove characters from a string starting from zero up to n and return a new string.",8)
+
+#2
+def remove_str (word,x,y):
+    print("Orgin:", word)
+    remove_word = word[x:y]
+    print(remove_word)
+
+remove_str("Write a program to remove characters from a string starting from zero up to n and return a new string.",8,-13)
+
