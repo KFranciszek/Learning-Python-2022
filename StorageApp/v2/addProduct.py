4714013504729e96ddc09a7b22e98bb0e5c4d275
KFranciszek <kfzmarketing@gmail.com>
2022-12-31 22:32:12 +0100

    add StorageApp v2/ addProduct with json file

--- StorageApp/v2/addProduct.py
+++ StorageApp/v2/addProduct.py
@@ -1,4 +1,21 @@
-import  json
-class AddProduct():
-    def __init__(self):
-        self.prods_dict  = {}
+import json
+prod_list = []
+def add_prod(prod_list):
+    name = input("Add product name: ")
+    price = float(input("Add product price: "))
+    ammount = int(input("Add product ammount: "))
+    prod_list.append({"name": name, "price": price, "amount": ammount})
+try:
+    with open("prod_list.json", "r") as f:
+        prod_list = json.load(f)
+except:
+    pass
+
+
+def save_prod_do(prod_list):
+   with open("prod_list.json", "w") as f:
+        json.dump(prod_list, f)
+
+
+add_prod(prod_list)
+save_prod_do(prod_list)
