370eb59fc44c5da7db29f68d752775bb7cff9a6d
KFranciszek <kfzmarketing@gmail.com>
2023-01-01 16:20:43 +0100

    searchProduct.py updatecode

--- StorageApp/v2/searchProduct.py
+++ StorageApp/v2/searchProduct.py
@@ -1,13 +1,30 @@
 import  json
+import  sys
 def search_product():
     search_select = input("What product you looking: ")
-    with open("prod_list.json", 'r') as f:
-        data_open = f.read()
-        data_search = json.loads(data_open)
+    try:
+        with open("prod_list.json", 'r') as f:
+
+
+            data_open = f.read()
+            data_search = json.loads(data_open)
+
+    except FileNotFoundError:
+        print("File not found")
+        sys.exit(1)
+
+    except  ValueError:  # includes simplejson.decoder.JSONDecodeError
+        print('Decoding JSON has failed')
+
+    found = False
     for i in data_search:
         if i["name"] == search_select:
             print(i)
-    else:
-        print("Product not find")
+            found = True
+            break
+
+    if not found:
+       print("Product dont found")
+
 
 search_product()
