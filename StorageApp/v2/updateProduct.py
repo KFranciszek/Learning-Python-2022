d91c960285d4c126b1ca94956674911cd6272186
KFranciszek <kfzmarketing@gmail.com>
2023-01-04 17:03:03 +0100

    add new function updateProduct.py

--- StorageApp/v2/updateProduct.py
+++ StorageApp/v2/updateProduct.py
@@ -1,2 +1,43 @@
 import  json
+import  sys
 def update_product():
+    update_select = input("What product you want  update: ")
+
+    try:
+        with open("prod_list.json", 'r') as f:  #We check that the product is on file without changing its
+
+            data_open = f.read()
+            data_search = json.loads(data_open)
+
+    except FileNotFoundError:
+        print("File not found")
+        sys.exit(1)
+
+    except  ValueError:  # includes simplejson.decoder.JSONDecodeError
+        print('Decoding JSON has failed')
+
+    found = False
+    update = ""
+    fild_update = ['name','price','amount']
+    value_upadate = ""
+    for i in data_search:
+        if i["name"] == update_select:
+            update= input(f"Are you sure you want to remove {i}  ? ")
+            found = True
+            if update == "yes":
+                fild_update_i = input("Witch fild update:? ")
+                if fild_update_i  in fild_update:
+                    value_upadate = input("Enter new value: ")
+                    i[fild_update_i] = value_upadate
+                    print(i)
+
+    with open("prod_list.json", 'w') as f:
+        json.dump(data_search, f)
+
+
+
+
+    if not found:
+       print("Product dont found")
+
+update_product()
