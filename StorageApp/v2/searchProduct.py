6159186796e58ec6ae171eb51a7bcc4ec91871aa
KFranciszek <kfzmarketing@gmail.com>
2023-01-05 20:44:15 +0100

    add to V2 searchProduct.py fix search method, possibility to search for multiple products with the same name

--- StorageApp/v2/searchProduct.py
+++ StorageApp/v2/searchProduct.py
@@ -4,8 +4,6 @@ def search_product():
     search_select = input("What product you looking: ")
     try:
         with open("prod_list.json", 'r') as f:
-
-
             data_open = f.read()
             data_search = json.loads(data_open)
 
@@ -17,11 +15,14 @@ def search_product():
         print('Decoding JSON has failed')
 
     found = False
-    for i in data_search:
-        if i["name"] == search_select:
-            print(i)
-            found = True
-            break
+    search_list = []
+    while True:
+        for i in data_search:
+            if i["name"] == search_select:
+                search_list.append(i)
+                found = True
+        print(search_list)
+        break
 
     if not found:
        print("Product dont found")
