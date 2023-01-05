3e054a028249c2b6ab691b3a3ff4926b7bd556c6
KFranciszek <kfzmarketing@gmail.com>
2023-01-05 23:59:22 +0100

    add to V3 addProduct.py

--- StorageApp/v3/addProduct.py
+++ StorageApp/v3/addProduct.py
@@ -1,19 +1,21 @@
-class AddProduct():
-    def __init__(self):
-        self.prods_list  = []
+import json
+prod_list = []
+def add_prod(prod_list):
+    name = input("Add product name: ")
+    price = float(input("Add product price: "))
+    ammount = int(input("Add product ammount: "))
+    prod_list.append({"name": name, "price": price, "amount": ammount})
+try:
+    with open("prod_list.json", "r") as f:
+        prod_list = json.load(f)
+except:
+    pass
 
-    def add_prod(self, prod):
-        self.prods_list.append(prod)
 
-    def save_file_prod(self):
-        with open("products_list.txt", "a") as f:
-            for i in self.prods_list:
-                f.write(i +"\n")
+def save_prod_do(prod_list):
+   with open("prod_list.json", "w") as f:
+        json.dump(prod_list, f)
 
-add_prod = AddProduct()
-while True:
-    insert_prod = input("Enter product (enter 'q' to quit): ").lower()
-    if insert_prod == "q":
-        break
-    add_prod.add_prod(insert_prod)
-add_prod.save_file_prod()
+
+add_prod(prod_list)
+save_prod_do(prod_list)
