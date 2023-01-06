fefffdd2b79e331a92d1a17847abb5b42e9b5dd6
KFranciszek <kfzmarketing@gmail.com>
2023-01-06 23:22:05 +0100

    add fix  V3 addProduct.py

--- StorageApp/v3/addProduct.py
+++ StorageApp/v3/addProduct.py
@@ -1,21 +1,41 @@
 import json
-prod_list = []
+import sys
 def add_prod(prod_list):
-    name = input("Add product name: ")
-    price = float(input("Add product price: "))
-    ammount = int(input("Add product ammount: "))
-    prod_list.append({"name": name, "price": price, "amount": ammount})
-try:
-    with open("prod_list.json", "r") as f:
-        prod_list = json.load(f)
-except:
-    pass
+    try:
+        with open("prod_list.json", "r") as f:
+            prod_list = json.load(f)
+
+    except (FileNotFoundError, ValueError,AttributeError,Exception) as e:
+        print(f"An error occurred while reading the file: {e}")
 
 
-def save_prod_do(prod_list):
-   with open("prod_list.json", "w") as f:
-        json.dump(prod_list, f)
+    try:
 
+        name = input("Add product name: ")
+        for product in prod_list:
+            if product["name"] == name:
+                print("Product already exists in the list.")
+                return
+
+            else:
+
+                price = float(input("Add product price: "))
+                ammount = int(input("Add product ammount: "))
+                prod_list.append({"name": name, "price": price, "amount": ammount})
+                print(prod_list)
+            break
+
+
+    except Exception as e1:
+        print(f"An error occurred: {e1}")
+
+    if len(prod_list) == 0:
+        return
+    else:
+        with open("prod_list.json", "w") as f:
+            json.dump(prod_list, f)
+            return prod_list
 
 add_prod(prod_list)
-save_prod_do(prod_list)
+
+
