27c51c2f107e32f86f50a54d689e9b8779fe4947
KFranciszek <kfzmarketing@gmail.com>
2023-01-09 12:29:11 +0100

    add to v4: addProduct.py (first version def)

--- StorageApp/v4/addProduct.py
+++ StorageApp/v4/addProduct.py
@@ -1,3 +1,25 @@
 import sqlite3
-def add_prod(prod_list):
-    #in progress
+import  random
+def addProduct():
+    conn = sqlite3.connect('databse_storage.db')
+    cursor = conn.cursor()
+    name = input("Add product name: ").lower()
+    cursor.execute(f'SELECT * FROM products WHERE name = "{name}"')
+    check_duble =    cursor.fetchall()
+    if len(check_duble) == 0:
+        price = float(input("Add product price: "))
+        amount = int(input("Add product ammount: "))
+        barcode = ''.join(name + "_" + str(random.randint(10000,99999)))
+        cursor.execute('INSERT INTO products (name,price,amount,barcode) VALUES (?,?,?,?)',(name,price,amount,barcode))
+    else:
+        print("Product on the list")
+
+    conn.commit()
+    conn.close()
+
+addProduct()
+
+
+
+
+
