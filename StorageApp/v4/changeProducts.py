7d7f3919bccd8a707672ff4dcc404b3aa2db2ed0
KFranciszek <kfzmarketing@gmail.com>
2023-01-11 15:48:08 +0100

    add to v4: changeProducts.py

---
+++ StorageApp/v4/changeProducts.py
@@ -0,0 +1,33 @@
+import sqlite3
+def changeProduct():
+    conn = sqlite3.connect('databse_storage.db')
+    cursor = conn.cursor()
+    name = input("What produkt you want to change? ")
+    cursor.execute(f'SELECT * FROM products WHERE name = "{name}"')
+    change_product_list = cursor.fetchall()
+    print(change_product_list)
+    if len(change_product_list ) > 1:
+        chose_prod =input(f"Witch {name} update, tap barcode? ")
+        fields = ['name','price','amount']
+        chose_field = input(f"Witch fild you want to update:  name/price/amount/?")
+        update_value = input("Tap update value")
+        if chose_field in fields:
+            cursor.execute(f'UPDATE products SET {chose_field} = "{update_value}" WHERE barcode = "{chose_prod}"')
+
+    else:
+        fields = ['name','price','amount']
+        chose_field = input(f"Witch fild you want to update:  name/price/amount/?")
+        update_value = input("Tap update value")
+        if chose_field in fields:
+            cursor.execute(f'UPDATE products SET {chose_field} = "{update_value}" WHERE name = "{name}"')
+
+    conn.commit()
+    conn.close()
+
+
+
+
+
+
+
+changeProduct()
