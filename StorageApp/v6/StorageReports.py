becad16b746ce4c73f5f6b1348fc3e6fab1feb53
KFranciszek <kfzmarketing@gmail.com>
2023-01-23 19:50:27 +0100

    add fix to v6.

---
+++ StorageApp/v6/StorageReports.py
@@ -0,0 +1,23 @@
+from StorageApp.v5.dataBase_def import dataBases
+
+class StorageReports(dataBases):
+    def __init__(self):
+        self.base='databse_storage.db'
+        self.open_connection(self.base)
+
+    def report_products(self):
+
+
+    def decline_products(self):
+
+
+    def alert_products(self):
+
+
+
+
+
+
+
+
+
